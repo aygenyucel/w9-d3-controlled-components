a4e84d600ae9a6939ef3218a26b4665b35e2aaa6
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-24 01:29:48 +0300

    created controlled input and searching books

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -4,7 +4,7 @@ import "./SingleBook.css";
 function SingleBook(props) {
   let { singleBookObject } = props;
   return (
-    <Col xs={6} md={4} lg={3} className="mb-3">
+    <Col xs={6} md={4} lg={3} className="mb-3" key={singleBookObject.asin}>
       <Card>
         <Card.Img variant="top" src={singleBookObject.img} />
         <Card.Body className="d-flex align-items-center justify-content-center">
