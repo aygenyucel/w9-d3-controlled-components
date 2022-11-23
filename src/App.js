623057e04875ae9c6b1263b9e245e4deba4c32b9
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-23 23:58:27 +0300

    Created badge and alert components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import WarningSign from "./components/WarningSign";
+import MyBadge from "./components/MyBadge";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <WarningSign />
+      <MyBadge />
+    </div>
+  );
 }
 
 export default App;
