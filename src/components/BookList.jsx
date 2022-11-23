80d3e9b62cda4ab030e91c8f66fbeaa16cebcb46
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-24 00:44:37 +0300

    Added search form component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,4 +1,4 @@
-import { Container, Row } from "react-bootstrap";
+import { Button, Container, Form, Row } from "react-bootstrap";
 import fantasyBooks from "../booksData/fantasy.json";
 import SingleBook from "./SingleBook";
 
@@ -8,6 +8,14 @@ const BookList = (props) => {
     <Container>
       <hr />
       <div>Fantasy Books:</div>
+      <br />
+      <div>
+        <Form className="d-flex">
+          <Form.Control type="text" placeholder="Search in Fantasy books..." />
+          <Button variant="primary">Search</Button>
+        </Form>
+      </div>
+      <br />
       <Row>
         {listOfBooks.map((book) => (
           <SingleBook singleBookObject={book} />
